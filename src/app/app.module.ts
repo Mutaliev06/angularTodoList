c1c01116d3e3fd7718eab352a58b598ec042dccd
mutaliev06 <maga_mutaliev06@mail.ru>
2023-03-06 14:08:58 +0300

    + filter and addTodo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,24 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from "@angular/forms";
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ListComponent } from './components/list/list.component';
-import { AddTaskComponent } from './components/add-task/add-task.component';
 import { ItemComponent } from './components/item/item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ListComponent,
-    AddTaskComponent,
     ItemComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/item/item.component.ts
+++ src/app/components/item/item.component.ts
@@ -9,6 +9,8 @@ import {Todo, todos} from "../../../todo";
 export class ItemComponent implements OnInit{
   todos:Todo[]
 
+  inputTodo:string = "";
+
   constructor() {
     this.todos = [];
   }
@@ -23,5 +25,34 @@ export class ItemComponent implements OnInit{
 
       return todo;
     })
+  }
+
+  deleteTodo (id: number) {
+    this.todos = this.todos.filter((todo, i) => i !== id)
+  }
+
+  addTodo () {
+    if (this.inputTodo !== '') {
+      this.todos = todos;
+      todos.push({
+        text: this.inputTodo,
+        date: '2023',
+        completed: false
+      });
+    }
+
+    this.inputTodo = "";
+  }
+
+  allTodo () {
+    this.todos = todos;
+  }
+
+  doneTodo () {
+    this.todos = todos.filter(done => done.completed)
+  }
+
+  notDoneTodo () {
+    this.todos = todos.filter(done => !done.completed)
   }
 }
